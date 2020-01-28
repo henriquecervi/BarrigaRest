60957ebda22948a648820ea6c1edc6c50bbd57a9
henrique.cutri <henrique.cutri@SKAR1200658.rsinet.com.br>
2020-01-28 17:19:43 -0300

    Teste 1 e teste 2. Não deve acessar API sem Token; Deve incluir conta com sucesso.

--- BarrigaRest/src/main/java/br/cervi/henrique/rest/tests/BarrigaTest.java
+++ BarrigaRest/src/main/java/br/cervi/henrique/rest/tests/BarrigaTest.java
@@ -2,6 +2,9 @@ package br.cervi.henrique.rest.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import br.cervi.henrique.rest.core.BaseTest;
@@ -20,5 +23,40 @@ public class BarrigaTest extends BaseTest {
 		;
 	}
 	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "wagner@aquino");
+		login.put("senha", "123456");
+		
+		
+		String token = given()
+			.body(login)
+//			.formParam("email", "henrique@henrique")
+//			.formParam("senha", "123456")
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+		
+		
+//		System.out.println(token);
+//		System.out.println("--------------------------------");
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{\"nome\": \"conta henrique\" }")
+		.when()
+			.post("/contas")
+		.then()
+			.log().all()
+			.statusCode(201)
+		;
+	}
+	
 
 }
+
+// criando json para o eclipse incluir as barras.
+// { "nome": "conta qualquer" }
