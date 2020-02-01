8729fbdfe99eb117b37d357978e2b95a4b138ba2
henrique.cutri <henrique.cutri@SKAR1200658.rsinet.com.br>
2020-02-01 16:13:31 -0300

    Não deve incluir conta com nome repetido.

--- BarrigaRest/src/main/java/br/cervi/henrique/rest/tests/BarrigaTest.java
+++ BarrigaRest/src/main/java/br/cervi/henrique/rest/tests/BarrigaTest.java
@@ -1,16 +1,40 @@
 package br.cervi.henrique.rest.tests;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.cervi.henrique.rest.core.BaseTest;
 
 
 public class BarrigaTest extends BaseTest {
+	
+	private String TOKEN;
+	
+	@Before
+	public void login () {
+		
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "henrique@henrique");
+		login.put("senha", "123456");
+		
+		TOKEN = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+	}
+	
 	
 	@Test
 	public void naoDeveAcessarSemToken() {
@@ -25,28 +49,9 @@ public class BarrigaTest extends BaseTest {
 	
 	@Test
 	public void deveIncluirContaComSucesso() {
-		Map<String, String> login = new HashMap<>();
-		login.put("email", "henrique@henrique");
-		login.put("senha", "123456");
-		
-		
-		String token = given()
-			.body(login)
-//			.formParam("email", "henrique@henrique")
-//			.formParam("senha", "123456")
-		.when()
-			.post("/signin")
-		.then()
-			.log().all()
-			.statusCode(200)
-			.extract().path("token");
-		
-		
-//		System.out.println(token);
-//		System.out.println("--------------------------------");
 		
 		given()
-			.header("Authorization", "JWT " + token)
+			.header("Authorization", "JWT " + TOKEN)
 			// criando json para o eclipse incluir as barras.
 			// { "nome": "Conta Henrique" }
 			.body("{\"nome\": \"Conta Bruna\" }")
@@ -60,33 +65,35 @@ public class BarrigaTest extends BaseTest {
 	
 	@Test
 	public void deveAlterarContaComSucesso() {
-		//praticamente mesmo processo do acima.
-		Map<String, String> login = new HashMap<>();
-		login.put("email", "henrique@henrique");
-		login.put("senha", "123456");
-		
-		String token = given()
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.log().all()
-			.statusCode(200)
-			.extract().path("token")
-		;
+		//praticamente mesmo processo do acima.		
 		
 		given()
-			.header("Authorization", "JWT " + token)
+			.header("Authorization", "JWT " + TOKEN)
 			.body("{\"nome\": \"Conta Rique\" }")
 		.when()
 			.put("/contas/57966")
 		.then()
 			.log().all()
 			.statusCode(200)
+			.body("nome", is("Conta Rique"))
 		;
 	}
 	
+	@Test
+	public void naoDeveIncluirContaComNomeRepetido() {
+		//praticamente mesmo processo do acima.		
+		
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{\"nome\": \"Conta Rique\" }")
+		.when()
+			.post("/contas/")
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("error", is("Já existe uma conta com esse nome!"))
+			;
+
+	}
 
 }
-
-
